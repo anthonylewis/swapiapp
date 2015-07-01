53dbd1a5e7d5544d69016469663bdd9dab66c2bc
Anthony Lewis <me@anthonylewis.com>
2015-06-30 20:37:53 -0500

    Add FilmService

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 (function () {
   var app = angular.module('SwapiApp', []);
 
-  app.controller('FilmsController', ['$http', function($http) {
+  app.factory('FilmService', ['$http', function($http) {
+    return {
+      getFilms: function() {
+        return $http.get('http://swapi.co/api/films/');
+      }
+    }
+  }]);
+
+  app.controller('FilmsController', ['FilmService', function(FilmService) {
     var films = this;
 
-    $http.get('http://swapi.co/api/films/')
+    FilmService.getFilms()
       .success(function(data) {
         films.filmList = data.results;
       });

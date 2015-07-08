34dce8a0f5b10130b7c034a064449f2cbe8900e7
Anthony Lewis <me@anthonylewis.com>
2015-07-07 20:16:13 -0500

    Add people

--- js/app.js
+++ js/app.js
@@ -9,6 +9,14 @@
     }
   }]);
 
+  app.factory('PeopleService', ['$http', function($http) {
+    return {
+      getPerson: function(id) {
+        return $http.get('http://swapi.co/api/people/' + id + '/');
+      }
+    }
+  }]);
+
   app.controller('FilmsController', ['FilmService', function(FilmService) {
     var films = this;
 
@@ -17,4 +25,14 @@
         films.filmList = data.results;
       });
   }]);
+
+  app.controller('PeopleController', ['PeopleService', function(PeopleService) {
+    var people = this;
+
+    PeopleService.getPerson(1)
+      .success(function(data) {
+        people.person = data;
+      });
+  }]);
+
 })();

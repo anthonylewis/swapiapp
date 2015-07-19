58c8e21bb31fc42867100b539f1fdb25c5de3a57
Anthony Lewis <alewis@sharethrough.com>
2015-07-18 18:53:07 -0700

    Clean up formatting a bit

--- js/controllers.js
+++ js/controllers.js
@@ -1,66 +1,66 @@
 (function () {
-  angular.module('SwapiApp').controller('FilmsController', ['ApiService', function(ApiService) {
+  angular.module('SwapiApp').controller('FilmsController', ['ApiService', function (ApiService) {
     var films = this;
 
     films.resource = 'films';
 
     ApiService.getAll(films.resource)
-      .success(function(data) {
+      .success(function (data) {
         films.filmList = data.results;
       });
   }]);
 
-  angular.module('SwapiApp').controller('PeopleController', ['ApiService', function(ApiService) {
+  angular.module('SwapiApp').controller('PeopleController', ['ApiService', function (ApiService) {
     var people = this;
 
     people.resource = 'people';
 
     ApiService.getOne(people.resource, 1)
-      .success(function(data) {
+      .success(function (data) {
         people.person = data;
       });
   }]);
 
-  angular.module('SwapiApp').controller('PlanetsController', ['ApiService', function(ApiService) {
+  angular.module('SwapiApp').controller('PlanetsController', ['ApiService', function (ApiService) {
     var planets = this;
 
     planets.resource = 'planets';
 
     ApiService.getOne(planets.resource, 1)
-      .success(function(data) {
+      .success(function (data) {
         planets.planet = data;
       });
   }]);
 
-  angular.module('SwapiApp').controller('SpeciesController', ['ApiService', function(ApiService) {
+  angular.module('SwapiApp').controller('SpeciesController', ['ApiService', function (ApiService) {
     var species = this;
 
     species.resource = 'species';
 
     ApiService.getOne(species.resource, 1)
-      .success(function(data) {
+      .success(function (data) {
         species.species = data;
       });
   }]);
 
-  angular.module('SwapiApp').controller('StarshipsController', ['ApiService', function(ApiService) {
+  angular.module('SwapiApp').controller('StarshipsController', ['ApiService', function (ApiService) {
     var starships = this;
 
     starships.resource = 'starships';
 
     ApiService.getOne(starships.resource, 10)
-      .success(function(data) {
+      .success(function (data) {
         starships.starship = data;
       });
   }]);
 
-  angular.module('SwapiApp').controller('VehiclesController', ['ApiService', function(ApiService) {
+  angular.module('SwapiApp').controller('VehiclesController', ['ApiService', function (ApiService) {
     var vehicles = this;
 
     vehicles.resource = 'vehicles';
 
     ApiService.getOne(vehicles.resource, 4)
-      .success(function(data) {
+      .success(function (data) {
         vehicles.vehicle = data;
       });
   }]);
--- js/services.js
+++ js/services.js
@@ -1,10 +1,10 @@
 (function () {
-  angular.module('SwapiApp').factory('ApiService', ['$http', function($http) {
+  angular.module('SwapiApp').factory('ApiService', ['$http', function ($http) {
     return {
-      getOne: function(resource, id) {
+      getOne: function (resource, id) {
         return $http.get('http://swapi.co/api/' + resource + '/' + id + '/');
       },
-      getAll: function(resource) {
+      getAll: function (resource) {
         return $http.get('http://swapi.co/api/' + resource + '/');
       }
     }
